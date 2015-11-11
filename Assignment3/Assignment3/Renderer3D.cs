470fc33e06b830805a56b3d535c2fb5cc53cf9af
Farga Krishnanta <f_krishnanta@live.com>
2015-11-11 03:30:24 -0800

    Update ambient lighting

--- Assignment3/Assignment3/Renderer3D.cs
+++ Assignment3/Assignment3/Renderer3D.cs
@@ -22,12 +22,12 @@ namespace Assignment3
         float lightIntensity = 1;
         public void Day()
         {
-            lightIntensity = 1;
+            lightIntensity = 20;
         }
 
         public void Night()
         {
-            lightIntensity = 0.1f;
+            lightIntensity = 0.3f;
         }
 
         public void Render(View view){
@@ -54,8 +54,8 @@ namespace Assignment3
                         effect.LightingEnabled = true;
                         effect.DirectionalLight0.DiffuseColor = new Vector3(1, 1, 1);
                         effect.AmbientLightColor = new Vector3(lightIntensity, lightIntensity, lightIntensity);
-                        effect.DirectionalLight0.Direction = new Vector3(0, 1, 0);
-                        effect.DirectionalLight0.SpecularColor = new Vector3(0, 1, 0);
+                        effect.DirectionalLight0.Direction = new Vector3(0, 0, 0);
+                        effect.DirectionalLight0.SpecularColor = new Vector3(0, 0, 0);
                         Game1.graphics.GraphicsDevice.SamplerStates[0] = SamplerState.LinearClamp;
                     }
 
